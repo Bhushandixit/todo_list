d7298581baf33118d0f7812d2fd6ab202091028b
Sanskar Pandit <codewithsanskar@gmail.com>
2023-09-29 23:16:40 +0530

    Applied styling on Add button

--- main.js
+++ main.js
@@ -6,11 +6,3 @@ function addList() {
     document.getElementById("lists").innerHTML = userValue;
   }
 }
-var newlist = document.getElementsByTagName("LI");
-for (var i = 0; i < newlist.length; i++) {
-  var span = document.createElement("SPAN");
-  var text = document.createTextNode("Remove");
-  span.className = "closeTag";
-  span.appendChild(text);
-  newlist[i].appendChild(span);
-}
